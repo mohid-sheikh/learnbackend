2bebbe99a8aca24bc0e91909b22cfabeeedf0087
Mohid-sheikh <mohidsheikh3817@gmail.com>
2026-05-05 17:25:09 +0530

    github data added

--- index.js
+++ index.js
@@ -3,7 +3,41 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 const port = 3000
-
+const msgithubdata={
+  "login": "mohid-sheikh",
+  "id": 230835417,
+  "node_id": "U_kgDODcJE2Q",
+  "avatar_url": "https://avatars.githubusercontent.com/u/230835417?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/mohid-sheikh",
+  "html_url": "https://github.com/mohid-sheikh",
+  "followers_url": "https://api.github.com/users/mohid-sheikh/followers",
+  "following_url": "https://api.github.com/users/mohid-sheikh/following{/other_user}",
+  "gists_url": "https://api.github.com/users/mohid-sheikh/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/mohid-sheikh/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/mohid-sheikh/subscriptions",
+  "organizations_url": "https://api.github.com/users/mohid-sheikh/orgs",
+  "repos_url": "https://api.github.com/users/mohid-sheikh/repos",
+  "events_url": "https://api.github.com/users/mohid-sheikh/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/mohid-sheikh/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": null,
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 1,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 0,
+  "created_at": "2025-09-07T10:11:29Z",
+  "updated_at": "2026-05-05T08:52:09Z"
+}
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -16,6 +50,9 @@ app.get('/instagram', (req, res) => {
 
 app.get('/login',(req,res)=>{
     res.send('<h1>please subscribe to my  intragram</h1>')
+})
+app.get('/github',(req,res)=>{
+  res.json(msgithubdata)
 })
 
 app.listen(process.env.PORT, () => {
